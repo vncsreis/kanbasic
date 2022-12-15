3696d5e0c0932963dbcfb12034ab6a2760344d6c
me <me@example.com>
2022-12-15 17:09:13 -0300

    url fix attempt

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import App from '../App';
-import HomePage from '../pages/HomePage';
-import ProjectPage from '../pages/ProjectPage';
+import { Route, Routes } from "react-router-dom";
+import App from "../App";
+import HomePage from "../pages/HomePage";
+import ProjectPage from "../pages/ProjectPage";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<App />}>
+      <Route path="/kanbasic" element={<App />}>
         <Route path="/:project" element={<ProjectPage />} />
         <Route index element={<HomePage />} />
       </Route>
